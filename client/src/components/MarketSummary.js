63816e71997dce710d6ea687c3052a4106ab70fd
rmv6471 <ryanvu6471@gmail.com>
2024-09-10 00:26:28 -0700

    Add Market Summary component for dashboard insights

---
+++ client/src/components/MarketSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MarketSummary = () => {
+    return (
+        <div>
+            <h2>Market Summary</h2>
+            {/* Placeholder for market summary data */}
+        </div>
+    );
+};
+
+export default MarketSummary;
