cc9b4bac12994a12de0a02ac6e726e93c1765356
rmv6471 <ryanvu6471@gmail.com>
2024-09-10 16:51:59 -0700

    Modify HomePage to include StockWidget for sentiment display

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,16 @@
 // src/pages/HomePage.js
 import React from 'react';
+import StockWidget from '../components/StockWidget';
 
-const Home = () => {
-  return <div></div>;
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <StockWidget name="Tesla Inc" ticker="TSLA" sentiment={1.24} />
+      <StockWidget name="Apple Inc" ticker="AAPL" sentiment={-0.76} />
+      {/* Add more widgets or other components as needed */}
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
