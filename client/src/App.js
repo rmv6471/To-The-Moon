02ee36fe507d0fadca95a4532a8143993c0dc5f7
rmv6471 <ryanvu6471@gmail.com>
2024-09-10 00:28:39 -0700

    Refactor App.js to include new routing and component integrations

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
+// src/App.js
 import React from 'react';
-import Header from './components/Header'; // Import the Header component
-import HomePage from './pages/HomePage'; // Import the HomePage component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import NavigationBar from './components/NavigationBar';
+import Home from './pages/HomePage';
+import News from './pages/NewsPage';  // Assume you have this
+import Trends from './pages/TrendsPage';  // Assume you have this
+import About from './pages/AboutPage';
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
-      <main>
-        <HomePage /> 
-      </main>
-    </div>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/trends" element={<Trends />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 };
 
