623a7192fa8e1ac4c943098a2030e618aef45d03
rmv6471 <ryanvu6471@gmail.com>
2024-09-10 00:26:38 -0700

    Add Top Movers component to showcase leading stock movements

---
+++ client/src/components/TopMovers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopMovers = () => {
+    return (
+        <div>
+            <h2>Top Movers</h2>
+            {/* Placeholder for stocks with significant movement */}
+        </div>
+    );
+};
+
+export default TopMovers;
