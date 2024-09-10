5070778167004d299602e01c43bcfd54647241bb
rmv6471 <ryanvu6471@gmail.com>
2024-09-10 00:26:33 -0700

    Add Sentiment Ticker component to display real-time sentiment data

---
+++ client/src/components/SentimentTicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SentimentTicker = () => {
+    return (
+        <div>
+            <h2>Sentiment Ticker</h2>
+            {/* Placeholder for live sentiment ticker */}
+        </div>
+    );
+};
+
+export default SentimentTicker;
