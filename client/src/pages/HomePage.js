c0f978fe279ebfa49f43243fd50d91fd629d5454
rmv6471 <ryanvu6471@gmail.com>
2024-09-12 17:41:41 -0700

    Updated homepage with title of website, implemented specialized font, and description of the application

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,8 +6,13 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <div className="home-container">
-      <div className = "centered-content"></div>
-      
+      <div className="centered-content">
+        <p className ="to-the-text">To The</p>
+        <p className="moon-text">Moon</p>
+        <p className ="description-text">Invest in your favorite</p>
+        <p className = "description-text">stocks with the help</p>
+        <p className = "description-text">sentiment analysis.</p>
+      </div>
     </div>
   );
 };
